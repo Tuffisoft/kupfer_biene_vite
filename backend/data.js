eeec06abf293388891a6d6747aa4b88beff2cedb
Fi <vancetti@googlemail.com>
2022-10-26 19:24:54 +0200

    add sever with node.js

---
+++ backend/data.js
@@ -0,0 +1,27 @@
+const data = {
+    products: [{
+            name: "Sommertracht",
+            slug: "sommertracht",
+            kategorie: "honig",
+            image: "/images/sommertracht.png",
+            preis: 5.99,
+            countInStock: 10,
+            farbe: "hell",
+            geschmack: "kräftig",
+            menge: "500gr",
+        },
+        {
+            name: "Frühtracht",
+            slug: "frühtracht",
+            kategorie: "honig",
+            image: "/images/frühtracht.png",
+            preis: 5.99,
+            countInStock: 10,
+            farbe: "hell",
+            geschmack: "herb, würzig",
+            menge: "500gr",
+        }
+    ]
+};
+
+export default data;
