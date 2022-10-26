6b1a36021150cfa05441c9afd08bf07b2572f006
Fi <vancetti@googlemail.com>
2022-10-26 13:58:15 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+
+  return (
+    <div>
+      <header>
+        <a href="/"><img className="headerLogo" src='./logo.png'></img></a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  )
+}
+
+
---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+    products: [{
+        name: "Kupferbiene Honig Sommertracht",
+        slug: "kupferbiene-honig-sommertracht",
+        kategorie: "honig",
+        image: "/images/sommertracht.png",
+        preis: 5.99,
+        countInStock: 10,
+        farbe: "hell",
+        geschmack: "kräftig",
+        menge: "500gr",
+    } {
+        name: "Kupferbiene Honig Frühtracht",
+        slug: "kupferbiene-honig-frühtracht",
+        kategorie: "honig",
+        image: "/images/frühtracht.png",
+        preis: 5.99,
+        countInStock: 10,
+        farbe: "hell",
+        geschmack: "herb, würzig",
+        menge: "500gr",
+    }]
+}
